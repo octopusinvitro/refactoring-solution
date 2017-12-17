c52b923e242bdd4b5a24085d0afc088688bb909c
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-12-17 18:18:01 +0000

    Manual fix of remaining Rubocop errors

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe(DateRangeFormatter) do
   end
 
   it 'formats a date range for the same day with starting and ending times' do
-    formatter = DateRangeFormatter.new('2009-11-1', '2009-11-1', '10:00', '11:00')
+    formatter = DateRangeFormatter.new(
+      '2009-11-1', '2009-11-1', '10:00', '11:00'
+    )
     expect(formatter.to_s).to eq('1st November 2009 at 10:00 to 11:00')
   end
 
@@ -25,12 +27,18 @@ RSpec.describe(DateRangeFormatter) do
 
   it 'formats a date range for the same month with starting time' do
     formatter = DateRangeFormatter.new('2009-11-1', '2009-11-3', '10:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 3rd November 2009')
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 3rd November 2009'
+    )
   end
 
   it 'formats a date range for the same month with starting and ending times' do
-    formatter = DateRangeFormatter.new('2009-11-1', '2009-11-3', '10:00', '11:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 3rd November 2009 at 11:00')
+    formatter = DateRangeFormatter.new(
+      '2009-11-1', '2009-11-3', '10:00', '11:00'
+    )
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 3rd November 2009 at 11:00'
+    )
   end
 
   it 'formats a date range for the same year' do
@@ -40,12 +48,18 @@ RSpec.describe(DateRangeFormatter) do
 
   it 'formats a date range for the same year with starting time' do
     formatter = DateRangeFormatter.new('2009-11-1', '2009-12-1', '10:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 1st December 2009')
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 1st December 2009'
+    )
   end
 
   it 'formats a date range for the same year with starting and ending times' do
-    formatter = DateRangeFormatter.new('2009-11-1', '2009-12-1', '10:00', '11:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 1st December 2009 at 11:00')
+    formatter = DateRangeFormatter.new(
+      '2009-11-1', '2009-12-1', '10:00', '11:00'
+    )
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 1st December 2009 at 11:00'
+    )
   end
 
   it 'formats a date range for different year' do
@@ -55,11 +69,17 @@ RSpec.describe(DateRangeFormatter) do
 
   it 'formats a date range for different year with starting time' do
     formatter = DateRangeFormatter.new('2009-11-1', '2010-12-1', '10:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 1st December 2010')
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 1st December 2010'
+    )
   end
 
   it 'formats a date range for different year with starting and ending times' do
-    formatter = DateRangeFormatter.new('2009-11-1', '2010-12-1', '10:00', '11:00')
-    expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 1st December 2010 at 11:00')
+    formatter = DateRangeFormatter.new(
+      '2009-11-1', '2010-12-1', '10:00', '11:00'
+    )
+    expect(formatter.to_s).to eq(
+      '1st November 2009 at 10:00 - 1st December 2010 at 11:00'
+    )
   end
 end
