1f48e1cfa64e8ba923b00eb53948882151c7844f
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-12-18 00:06:19 +0000

    Refactoring: remove the conditional in the main method

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -4,6 +4,9 @@ require 'date'
 require 'fixnum'
 
 class DateRangeFormatter
+  MONTH = '%B'
+  YEAR = '%Y'
+
   def initialize(start_date, end_date, start_time = nil, end_time = nil)
     @start_date = Date.parse(start_date)
     @end_date = Date.parse(end_date)
@@ -12,18 +15,41 @@ class DateRangeFormatter
   end
 
   def to_s
-    if same_date?
-      same_date
-    else
-      different_date
-    end
+    "#{full_start_date}#{formatted_start_time}" \
+    "#{full_end_date_with_dash}#{formatted_end_time}"
   end
 
   private
 
   attr_reader :start_date, :end_date, :start_time, :end_time
 
-  def same_date?
+  def full_start_date
+    start_date.strftime("#{start_date.day.ordinalize}#{start_month_year}")
+  end
+
+  def full_end_date
+    end_date.strftime("#{end_date.day.ordinalize}#{end_month_year}")
+  end
+
+  def full_end_date_with_dash
+    " - #{full_end_date}" unless same_dates?
+  end
+
+  def formatted_start_time
+    " at #{start_time}" if start_time
+  end
+
+  def formatted_end_time
+    "#{preposition}#{end_time}" if end_time
+  end
+
+  def preposition
+    return ' at ' unless same_dates?
+    return ' to ' if start_time
+    ' until '
+  end
+
+  def same_dates?
     start_date == end_date
   end
 
@@ -35,49 +61,18 @@ class DateRangeFormatter
     start_date.year == end_date.year
   end
 
-  def same_date
-    if start_time && end_time
-      "#{full_start_date}#{full_start_time} to #{end_time}"
-    elsif end_time
-      "#{full_start_date}#{full_start_time} until #{end_time}"
-    else
-      "#{full_start_date}#{full_start_time}"
-    end
+  def a_time_was_provided?
+    start_time || end_time
   end
 
-  def different_date
-    "#{full_start_date}#{full_start_time} - #{full_end_date}#{full_end_time}"
+  def start_month_year
+    return " #{MONTH} #{YEAR}" if a_time_was_provided? || same_dates?
+    return '' if same_year? && same_month?
+    return " #{MONTH}" if same_year?
+    " #{MONTH} #{YEAR}"
   end
 
-  def full_start_date
-    start_date.strftime("#{start_date.day.ordinalize}#{month_and_year}")
-  end
-
-  def full_end_date
-    end_date.strftime("#{end_date.day.ordinalize} %B %Y")
-  end
-
-  def full_start_time
-    " at #{start_time}" if start_time
-  end
-
-  def full_end_time
-    " at #{end_time}" if end_time
-  end
-
-  def month_and_year
-    if start_time || end_time
-      ' %B %Y'
-    else
-      if same_date?
-        ' %B %Y'
-      elsif same_year? && same_month?
-        ''
-      elsif same_year?
-        ' %B'
-      else
-        ' %B %Y'
-      end
-    end
+  def end_month_year
+    " #{MONTH} #{YEAR}"
   end
 end
